2be3f78db3135b1f66f38f9391c498e85c19068d
nasubi916 <r916nis@gmai.com>
2024-04-13 00:12:25 +0900

    delete poetry

--- pylearn/main.py
+++ pylearn/main.py
@@ -15,6 +15,7 @@ def none() -> None:
     pass  # meaning return None
 
 
+# object define
 userInfo: dict[str, int | str] = {"name": "John", "age": 25}
 
 
@@ -26,4 +27,11 @@ class User:
 
 info: User = User(name="John", age=25)
 info.age = 26
-YouShouldLearn: str = "poetry install"
+
+# for i in range(0,200,10):
+#     print(i)
+
+array: list[int] = [1, 2, 3, 4, 5]
+text = "Hello, World!"
+print(array,text,sep=" ")
+
